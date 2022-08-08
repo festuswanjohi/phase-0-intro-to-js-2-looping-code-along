81dc82e012d64b2170a9bfd3cbca58896348c6b6
festus wanjohi <strategic.policyafrica@gmail.com>
2022-08-08 23:54:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const myArr = [];
+function writeCards(arr) {
+  for (const element of arr) {
+    const mess = `Thank you, ${element}, for the wonderful surprise gift!`;
+    myArr.push(mess);
+  }
+  return myArr;
+}
+function countDown(int) {
+  while (int >= 0) {
+    console.log(int);
+    int--;
+  }
+}
